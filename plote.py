f11b7cf9c226b79fde03def9a15ee46730f46a2d
Jasper Lammering <jlammering@me.com>
2015-12-07 14:49:23 +0100

    plot e jetzt richtig

--- plote.py
+++ plote.py
@@ -1,19 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-L = 1.75e-3
-C = 22e-9
-#x, y = np.genfromtxt('d_1.txt', unpack = True)
+x, y = np.genfromtxt('e.txt', unpack = True)
 
-def v_gr(nu):
-    return (1/(np.sqrt(L*C)))*np.sqrt(1-0.25*L*C*(nu*2*np.pi)**2)
-
-nu = np.linspace(0,100000,100000)
-
-plt.plot(nu, v_gr(nu), 'b-', label = 'Theoriekurve')
-plt.xlabel(r'$\nu \:/\: \si{\hertz}$')
-plt.ylabel(r'$v_{gr} \:/\: \si{\meter\per\second}$')
+plt.plot(x, y, 'rx', label = 'Messpunkte')
+plt.xlabel(r'Kettenglied')
+plt.ylabel(r'$U \:/\: \si{\volt}$')
 plt.legend(loc = 'best')
+plt.title(r'gemessen beim ersten Maximum : f = 5040 Hz mit Abschlusswiderstand = Wellenwid.')
 
 
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
